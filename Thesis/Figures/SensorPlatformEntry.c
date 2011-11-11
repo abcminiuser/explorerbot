31e8c73951d0b65e694b9bf058644a9ce16cb9d9
Dean Camera <dean@fourwalledcubicle.com>
2011-11-11 04:07:53 +0000

    Thesis: Add additional text for the Firmware Modules section. Remove Host Application User guide appendix (not needed). Update figures to use white borders around highlighted areas of photos to ensure clarity when printed in black and white.

--- Thesis/Figures/SensorPlatformEntry.c
+++ Thesis/Figures/SensorPlatformEntry.c
@@ -6,8 +6,7 @@ typedef struct
 	/* Human-readable name of the sensor, stored in SRAM. */
 	const char* Name;
 	
-	/* Indicates if the sensor data is represented as a single value,
-	   or a triplicate of three axis values. */
+	/* Indicates if the sensor data is represented as a single value, or a triplicate of three axis values. */
 	bool SingleAxis;
 	
 	/* Last retrieved data from the sensor. */	
@@ -16,7 +15,7 @@ typedef struct
 		/* Sensor data if the sensor outputs a single axis value. */
 		int32_t Single;
 	
-		/* Sensor data if the sensor outputs a triplicate of three axis values. */
+		/* Sensor data if the sensor outputs three axis values. */
 		struct
 		{
 			/* X axis value of the triplicate. */

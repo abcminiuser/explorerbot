68ed635783d4e210a12ea6d27b8b1531328b5e79
Dean Camera <dean@fourwalledcubicle.com>
2011-11-17 04:29:18 +0000

    Add additional top-down photo of the completed robot. Minor code style correction.

--- Code/Bluetooth/Bluetooth.h
+++ Code/Bluetooth/Bluetooth.h
@@ -31,7 +31,8 @@
 	/* Function Prototypes: */
 		void Bluetooth_Init(BT_StackConfig_t* const StackState);
 		bool Bluetooth_ManageConnections(BT_StackConfig_t* const StackState);
-		void Bluetooth_ProcessPacket(BT_StackConfig_t* const StackState, const uint8_t Type);
+		void Bluetooth_ProcessPacket(BT_StackConfig_t* const StackState,
+		                             const uint8_t Type);
 		void Bluetooth_TickElapsed(BT_StackConfig_t* const StackState);
 
 #endif
